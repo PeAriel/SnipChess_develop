77a02f84c4819caf748b04ec433ec9a06c351360
PeAriel <82453690+PeAriel@users.noreply.github.com>
2021-08-17 19:04:01 +0300

    Changed name to boards_png_generator and changed the structure so that the fen will be loaded only once

---
+++ boards_png_generator.py
@@ -0,0 +1,55 @@
+from fen2png import DrawBoard, RESOURCES
+import os
+from random import randint
+from tqdm import tqdm
+
+FENS = os.getcwd() + '/' + RESOURCES + 'fen_files/'
+
+def random_ints_list(max_int, length):
+    rand_ints = [0 for _ in range(length)]
+    for i in range(length):
+        rand_ints[i] = randint(0, max_int - 1)
+
+    return rand_ints
+
+def generate_pngs(themes, train=None, valid=None, verbosity=False):
+    if type(themes) is str:
+        themes = [themes]
+
+    with open(FENS + 'unique00.fen', 'r') as f:
+        print('Starting to read fen file.')
+        file_lines = f.read().splitlines()
+        print('Finished reading fen file.')
+    num_lines = len(file_lines)
+
+    for theme in themes:
+        if train:
+            random_lines_train = random_ints_list(num_lines, train)
+            if verbosity:
+                random_lines_train = tqdm(random_lines_train, desc='Training images completed: ')
+            for line in random_lines_train:
+                fen = file_lines[line].split()[0]
+                board = DrawBoard(fen, theme)
+                output_path = os.getcwd() + '/resources/training_dataset/{}'.format('S'.join(fen.split('/')))
+                board.save_board(output_path)
+        if valid:
+            random_lines_valid = random_ints_list(num_lines, valid)
+            if verbosity:
+                random_lines_valid = tqdm(random_lines_valid, desc='Validation images completed: ')
+            for line in random_lines_valid:
+                fen = file_lines[line].split()[0]
+                board = DrawBoard(fen, theme)
+                output_path = os.getcwd() + '/resources/validation_dataset/{}'.format('S'.join(fen.split('/')))
+                board.save_board(output_path)
+
+def gen_starting_position(output_path):
+    board = DrawBoard('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR', 'classic', square_size=40)
+    board.save_board(output_path)
+
+
+def main():
+    theme = ['classic']
+    generate_pngs(theme, train=int(1e3), valid=int(1e2), verbosity=True)
+
+if __name__ == '__main__':
+    main()
