3869632cf21190f091db8a9594615b7d502c439f
PeAriel <82453690+PeAriel@users.noreply.github.com>
2021-08-20 17:49:30 +0300

    Changed name from main.py to train_model.py

--- train_model.py
+++ train_model.py
@@ -1,27 +1,15 @@
-from datasets import ChessBoardsDataset, PiecesDataset
+from datasets import PiecesDataset
 from torch.utils.data import DataLoader
 from model import ChessConvNet
 
 import torch
-from torch.nn import L1Loss, MSELoss
+from torch.nn import CrossEntropyLoss
 from torch.optim import Adam
-import torchvision.models as models
 
 import os
 from tqdm import tqdm
 import matplotlib.pyplot as plt
 
-def load_parameters(net, pretrained_model):
-    pretrained_dict = pretrained_model.state_dict()
-    state_dict = net.state_dict()
-
-    for key in state_dict.keys():
-        if 'features' not in key:
-            continue
-        if key in pretrained_dict.keys():
-            state_dict[key] = pretrained_dict[key]
-
-    net.load_state_dict(state_dict)
 
 def compute_accuracy_and_loss(loss_func, dataloader, net):
     correct = 0
@@ -44,31 +32,25 @@ def compute_accuracy_and_loss(loss_func, dataloader, net):
             correct += len(torch.where(pred == target)[0])
             total += len(target)
 
-    total = nbatch * (8 ** 2 + 7)
     accuracy = correct / total
     loss /= nbatch
 
     return accuracy, loss
 
-def representation2fen(rep):
-    pass
-
 def main():
     if torch.cuda.is_available():
         os.system('echo Running on GPU.\n')
-    path_to_training_data = os.getcwd() + '/resources/training_dataset'
-    path_to_validation_data = os.getcwd() + '/resources/validation_dataset'
+    path_to_training_data = os.getcwd() + '/resources/training_dataset/pieces'
+    path_to_validation_data = os.getcwd() + '/resources/validation_dataset/pieces'
 
-    training_dataset = ChessBoardsDataset(path_to_training_data)
-    validation_dataset = ChessBoardsDataset(path_to_validation_data)
+    training_dataset = PiecesDataset(path_to_training_data)
+    validation_dataset = PiecesDataset(path_to_validation_data)
 
-    training_dataloader = DataLoader(training_dataset, batch_size=10, shuffle=True)  # need to shuffle since the files are orderd
-    validation_dataloader = DataLoader(validation_dataset, batch_size=10)
+    training_dataloader = DataLoader(training_dataset, batch_size=20, shuffle=True)  # need to shuffle since the files are orderd
+    validation_dataloader = DataLoader(validation_dataset, batch_size=20)
 
     net = ChessConvNet()
-    pretrained_model = models.vgg11(pretrained=True)
-    load_parameters(net, pretrained_model)
-    loss_function = MSELoss()
+    loss_function = CrossEntropyLoss()
     optimizer = Adam(net.parameters(), lr=0.001)
 
     if torch.cuda.is_available():
@@ -105,8 +87,9 @@ def main():
         validation_acc_vs_epoch.append(validation_accuracy)
         validation_loss_vs_epoch.append(validation_loss)
 
-        if len(validation_loss_vs_epoch) == 1 or validation_loss_vs_epoch[-1] < min(validation_loss_vs_epoch[:-1]):
-            torch.save(net.state_dict(), 'trained_model_primitive.pt')
+        if len(validation_loss_vs_epoch) > 1:
+            if validation_loss_vs_epoch[-1] < min(validation_loss_vs_epoch[:-1]):
+                torch.save(net.state_dict(), 'trained_model.pt')
 
     fig, ax = plt.subplots(1, 2, figsize=(8, 3))
 
@@ -119,7 +102,7 @@ def main():
     ax[1].legend(loc='upper left')
     ax[1].set_title('Accuracy vs epoch')
 
-    plt.savefig(os.getcwd() + '/loss_curve.png', dpi=500)
+    plt.savefig(os.getcwd() + '/loss_curve_classifier.png', dpi=500)
     plt.show()
 
 if __name__ == '__main__':
