01856541bd2d81a68dc1af0e4cb0f03d1d363261
PeAriel <82453690+PeAriel@users.noreply.github.com>
2021-08-20 17:47:00 +0300

    Now ChessConvNet is used to classify squares

--- model.py
+++ model.py
@@ -5,6 +5,44 @@ class ChessConvNet(Module):
     def __init__(self):
         super(ChessConvNet, self).__init__()
         self.in_channels = 3
+        self.out_vector_length = 13
+
+        self.features = Sequential(
+            Conv2d(self.in_channels, 10, kernel_size=(3, 3), padding=(1, 1), stride=(1, 1)),
+            ReLU(inplace=True),
+            Conv2d(10, 20, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
+            ReLU(inplace=True),
+            Conv2d(20, 20, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
+            ReLU(inplace=True),
+            Conv2d(20, 10, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
+            ReLU(inplace=True),
+            MaxPool2d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=False),
+            Conv2d(10, 10, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
+            ReLU(inplace=True),
+            MaxPool2d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=False)
+        )
+
+        self.classifier = Sequential(
+            Linear(in_features=4000, out_features=500, bias=True),
+            ReLU(inplace=True),
+            Linear(in_features=500, out_features=500, bias=True),
+            ReLU(inplace=True),
+            Linear(in_features=500, out_features=13, bias=True),
+        )
+
+
+
+    def forward(self, x):
+        out = self.features(x)
+        out = flatten(out, 1)
+        out = self.classifier(out)
+
+        return out
+
+class ChessBoardConvNet(Module):
+    def __init__(self):
+        super(ChessBoardConvNet, self).__init__()
+        self.in_channels = 3
         self.out_vector_length = 8 ** 2 + 7
 
         # ################ just testing for now ##########################
