c8b724eeb12f4f5225bd2e775c7582be9b51f4f1
PeAriel <82453690+PeAriel@users.noreply.github.com>
2021-09-01 23:31:05 +0300

    Added some comments to myself

---
+++ png2fen.py
@@ -0,0 +1,8 @@
+'''
+This should be the main output of the DL part.
+
+Thoughts:
+1. As for now, the default square size (after inflation is 80 pixels). For a faster
+   selective-search consider deflate the png after reading it from the user to get the squares,
+   and only than inflate to classify. This may be a bit problematic - TEST the inflatet square!!!
+'''
